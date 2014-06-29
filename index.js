c4ae7fe51001037332c705fc5768e0c81bd3f6de
Florent Vilmart <vfloz@me.com>
2014-06-29 12:23:03 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 var Parse = require("parse-cloud").Parse;
 module.exports = {
     initialize : function(api_user, api_key){
-        this.sendgrid = require('sendgrid')(api_user, api_key);
-    }
-    sendEmail: function(params, options){
-        var p = new Parse.Promise();
-        this.sendgrid.send(params, function(err, json){
-            if(err){
-                p.reject(err);
-                if(options && options.error){
-                    options.error(err);
-                }
-            }else{
-                p.resolve(json);
-                if(options && options.success){
-                    options.success(json);
-                }
-            }
-        });
+        this.api_user = api_user;
+        this.api_key = api_key;
+    },
+    sendEmail: function(params, cbOptions){
+        params.api_user = this.api_user;
+        params.api_key = this.api_key;
+        var options = {
+            url: "https://api.sendgrid.com/api/mail.send.json",
+            method: "POST",
+            body: params,
+            success: cbOptions && cbOptions.success || function(){},
+            error: cbOptions && cbOptions.error || function(){},
+        }
+
+        var p = Parse.Cloud.httpRequest(options);
+        console.log(p);
         return p;
     }
 }
